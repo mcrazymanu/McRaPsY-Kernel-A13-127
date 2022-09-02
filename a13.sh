d2caf3e73544aa526ea7ff0f16685306288acdf6
mcrazymanu <mcrazymanu@gmail.com>
2022-09-02 07:47:46 +0530

    Build kernel + AIk Build flashable zip

--- a13.sh
+++ a13.sh
@@ -8,3 +8,12 @@ export ANDROID_MAJOR_VERSION=s
 
 make ARCH=arm64 mcrazymanu_defconfig
 make ARCH=arm64 -j16
+
+cp -r arch/arm64/boot/Image AIK/Image
+cp -r arch/arm64/boot/dtb_exynos.img AIK/dtb
+cp -r arch/arm64/boot/dtbo_exynos.img AIK/dtbo
+cd AIK
+rm -rf McRaPsY_kernel_a13.zip
+rm -rf McRaPsY_kernel_a12s.zip
+zip -r9 McRaPsY_kernel_a13.zip * -x .git README.md *placeholder
+cd ..
