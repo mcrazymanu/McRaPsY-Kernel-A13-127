d58a3f79c888fe041d6ff43b431a9e01b3ad8fd8
mcrazymanu <mcrazymanu@gmail.com>
2022-09-03 05:49:53 +0530

    gaming_control: disable hotplug throttle

--- drivers/thermal/samsung/exynos_tmu_v2.c
+++ drivers/thermal/samsung/exynos_tmu_v2.c
@@ -43,6 +43,7 @@
 #include <linux/slab.h>
 #include <linux/debugfs.h>
 #include <linux/debug-snapshot.h>
+#include <linux/gaming_control.h>
 #include <soc/samsung/tmu.h>
 #include <soc/samsung/ect_parser.h>
 #ifdef CONFIG_EXYNOS_MCINFO
@@ -938,7 +939,7 @@ static int exynos_get_trend(void *p, int trip, enum thermal_trend *trend)
 	if (ret < 0)
 		return ret;
 
-	if (tz->temperature >= trip_temp)
+	if (tz->temperature >= trip_temp && !gaming_mode)
 		*trend = THERMAL_TREND_RAISE_FULL;
 	else
 		*trend = THERMAL_TREND_DROP_FULL;
@@ -1282,7 +1283,7 @@ static int exynos_throttle_cpu_hotplug(void *p, int temp)
 	temp = temp / MCELSIUS;
 
 	if (is_cpu_hotplugged_out) {
-		if (temp < data->hotplug_in_threshold) {
+		if (temp < data->hotplug_in_threshold || gaming_mode) {
 			/*
 			 * If current temperature is lower than low threshold,
 			 * call cluster1_cores_hotplug(false) for hotplugged out cpus.
@@ -1291,7 +1292,7 @@ static int exynos_throttle_cpu_hotplug(void *p, int temp)
 			is_cpu_hotplugged_out = false;
 		}
 	} else {
-		if (temp >= data->hotplug_out_threshold) {
+		if (temp >= data->hotplug_out_threshold && !gaming_mode) {
 			/*
 			 * If current temperature is higher than high threshold,
 			 * call cluster1_cores_hotplug(true) to hold temperature down.
